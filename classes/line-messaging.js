16ddcf8ea9e0f7b08782d472decd9176cc4c65a5
nuttakarn <nuttakarngg@gmail.com>
2021-05-19 16:34:20 +0700

    make it better

--- classes/line-messaging.js
+++ classes/line-messaging.js
@@ -22,9 +22,9 @@ class LineAPIService {
                     headers: headers,
                     body: body
                 }, (err, res, body) => {
-                    console.log('status = ' + {url: apiRoute,
+                    console.log('status = ' + JSON.stringify({url: apiRoute,
                         headers: headers,
-                        body: body});
+                        body: body}));
                     return resolve(res.statusCode);
                 });
             }
